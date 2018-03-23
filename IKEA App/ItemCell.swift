d92f7349c898c8613dba868cbb2bbd2dfe1e5169
Aly Mirza <a5mirza@uwaterloo.ca>
2018-03-22 20:00:37 -0400

    IKEA Furniture AR App

---
+++ IKEA App/ItemCell.swift
@@ -0,0 +1,14 @@
+//
+//  ItemCell.swift
+//  IKEA App
+//
+//  Created by Aly Mirza on 2017-11-14.
+//  Copyright © 2017 Aly Mirza. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UICollectionViewCell {
+    
+    @IBOutlet weak var itemLabel: UILabel!
+}
---
+++ IKEA AppTests/IKEA_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  IKEA_AppTests.swift
+//  IKEA AppTests
+//
+//  Created by Aly Mirza on 2017-11-14.
+//  Copyright © 2017 Aly Mirza. All rights reserved.
+//
+
+import XCTest
+@testable import IKEA_App
+
+class IKEA_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
